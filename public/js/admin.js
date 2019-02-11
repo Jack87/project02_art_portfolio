d6c7dbe24bcda88e59c03648a3dd20099efe14d1
Akop K <akop87@gmail.com>
2019-02-11 01:17:59 -0700

    dynamic gallery showing thumnails. Have bug with lightbox

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,39 +1,13 @@
-
-(function($) {
-    'use strict';
-
-    // Main Navigation
-    $( '.hamburger-menu' ).on( 'click', function() {
-        $(this).toggleClass('close');
-        $('.site-branding').toggleClass('hide');
-        $('.site-navigation').toggleClass('show');
-        $('.site-header').toggleClass('no-shadow');
-    });
-
-    // Scroll to Next Section
-    $( '.scroll-down' ).click(function() {
-        $( 'html, body' ).animate({
-            scrollTop: $( '.scroll-down' ).offset().top + 100
-        }, 800 );
-    });
-})(jQuery);
-
 $(document).ready(function() {
     // imageContainer holds all of our images
     var imageContainer = $(".image-container");
-    // var imageCategorySelect = $("#category");
     // Click events for the edit and delete buttons
     // $(document).on("click", "button.delete", handleImageDelete);
     // $(document).on("click", "button.edit", handleImageEdit);
     var images;
   
     // This function grabs images from the database and updates the view
-    // function getImages(category) {
     function getImages() {
-    //   var categoryString = category || "";
-    //   if (categoryString) {
-    //     categoryString = "/category/" + categoryString;
-    //   }
       $.get("/api/artwork", function(data) {
         console.log("Images", data);
         images = data;
@@ -58,14 +32,17 @@ $(document).ready(function() {
     // }
   
     // Getting the initial list of images
+
     getImages();
+    // doGallery();
+
     // InitializeRows handles appending all of our constructed image HTML inside
     // imageContainer
     function initializeRows() {
       imageContainer.empty();
       var imagesToAdd = [];
       for (var i = 0; i < images.length; i++) {
-        iamgesToAdd.push(createNewThumbnail(images[i]));
+        imagesToAdd.push(createNewThumbnail(images[i]));
       }
       imageContainer.append(imagesToAdd);
     }
@@ -73,17 +50,17 @@ $(document).ready(function() {
     // This function constructs a image's HTML
     function createNewThumbnail(image) {
         var thumbnail = $("<div>")
-            thumbnail.addClass("thumbnail");
+            thumbnail.addClass("thumbnail col-sm-6 col-md-4");
 
         var editBtn = $("<button>");
-            editBtn.addClass("edit btn btn-secondary");
-            editBtn.text("<i class='far fa-edit'></i>");
+            editBtn.addClass("editBtn btn btn-secondary");
+            editBtn.html("<i class='far fa-edit'></i>");
             
         var lightBox = $("<a>");
             lightBox.addClass("lightbox")
             lightBox.attr("href", image.imgURL)
         var imgSRC = $("<img>");
-            imgSRC.attr("href", image.imgURL);
+            imgSRC.attr("src", image.imgURL);
             imgSRC.attr("alt", image.title);
 
         var thumbnailCaption = $("<div>");
@@ -98,26 +75,23 @@ $(document).ready(function() {
             thumbnailDescription.addClass("description");
             thumbnailDescription.text("Description: " + image.description);
 
+        //Adding it all to DOM
+        thumbnail.append(editBtn);
+        thumbnail.append(lightBox);
+        lightBox.append(imgSRC)
+        thumbnail.append(thumbnailCaption);
+        thumbnailCaption.append(thumbnailTitle);
+        thumbnailCaption.append(thumbnailCategory);
+        thumbnailCaption.append(thumbnailDescription);
+        thumbnail.data("image", image);
+        baguetteBox.run('.tz-gallery');
+        return thumbnail;
+
 
     //   var deleteBtn = $("<button>");
     //   deleteBtn.text("x");
     //   deleteBtn.addClass("delete btn btn-danger");
-      
-    //   thumbnailCategory.css({
-    //     float: "right",
-    //     "font-weight": "700",
-    //     "margin-top":
-    //     "-15px"
-    //   });
-      thumbnailCaption.append(thumbnailTitle);
-      thumbnailCaption.append(thumbnailCategory);
-      thumbnailCaption.append(thumbnailDescription);
-      thumbnail.append(thumbnailCaption);
-      lightBox.append(imgSRC)
-      thumbnail.append(lightBox);
-      thumbnail.append(editBtn);
-      thumbnail.data("image", image);
-      return thumbnail;
+
     }
   
     // This function figures out which image we want to delete and then calls
@@ -149,11 +123,16 @@ $(document).ready(function() {
       imageContainer.append(messageH2);
     }
   
-    // This function handles reloading new images when the category changes
-    // function handleCategoryChange() {
-    //   var thumbnailCategory = $(this).val();
-    //   getImages(thumbnailCategory);
-    // }
-  
   });
+
+document.body.addEventListener('DOMSubtreeModified', function () {
+    // doGallery();
+    baguetteBox.run('.tz-gallery');   
+  }, false);
+
+  function doGallery() {
+    console.log("this just happened")
+    baguetteBox.run('.tz-gallery');
+  }
+
   
