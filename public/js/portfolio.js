d628d9806bf618e284dfd2a00a84b543c0ebc8fb
Brant Keener <brant_keener@yahoo.com>
2019-02-12 22:06:01 -0700

    Dynamic image load portfolio

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,52 +1,47 @@
-
-  var API = {
-    saveArtwork: function(artwork) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/artwork",
-        data: JSON.stringify(artwork)
-      });
-    },
-    saveCommissionRequest: function(commissionrequest) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/commission%20request",
-        data: JSON.stringify(commissionrequest)
-      });
-    },
-    getArtwork: function() {
-      return $.ajax({
-        url: "api/artwork",
-        type: "GET"
-      });
-    },
-    getCommissionRequest: function() {
-      return $.ajax({
-        url: "api/commission%20request",
-        type: "GET"
-      });
-    }
-    // Update functionality can be added as needed
-  };
-
-$(document).click(function(event) {
-  console.log(event.target);
-});
+var API = {
+  saveArtwork: function(artwork) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/artwork",
+      data: JSON.stringify(artwork)
+    });
+  },
+  saveCommissionRequest: function(commissionrequest) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "api/commission%20request",
+      data: JSON.stringify(commissionrequest)
+    });
+  },
+  getArtwork: function() {
+    return $.ajax({
+      url: "api/artwork",
+      type: "GET"
+    });
+  },
+  getCommissionRequest: function() {
+    return $.ajax({
+      url: "api/commission%20request",
+      type: "GET"
+    });
+  }
+  // Update functionality can be added as needed
+};
 
 function grabArtworkData() {
   API.getArtwork().then(function(res) {
     for (var i = 0; i < res.length; i++) {
-      var $columnDiv = $("<div>", { "class": "col-sm-12 col-md-2", id: [i] });
-      var $thumbnailDiv = $("<div>", { "class": "thumbnail" });
-      var $lightBoxA = $("<a>", { "class": "lightbox" });
+      var $columnDiv = $("<div>", { class: "col-sm-12 col-md-2", id: [i] });
+      var $thumbnailDiv = $("<div>", { class: "thumbnail" });
+      var $lightBoxA = $("<a>", { class: "lightbox" });
       var $img = $("<img style='margin:auto'>");
-      var $captionDiv = $("<div>", { "class": "caption" });
+      var $captionDiv = $("<div>", { class: "caption" });
       var $captionHeading = $("<h5 id=" + i + ">");
       var $captionPara = $("<p id=" + i + ">");
       $captionPara.text(res[i].medium);
@@ -60,7 +55,7 @@ function grabArtworkData() {
       $lightBoxA.append($img);
       $lightBoxA.attr({
         href: res[i].imgURL
-      })
+      });
       $thumbnailDiv.append($lightBoxA);
       $thumbnailDiv.append($captionDiv);
       $columnDiv.append($thumbnailDiv);
