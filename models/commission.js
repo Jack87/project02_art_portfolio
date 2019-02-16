16a066865d90e404fb1545ab502ce6f0c63ec74b
Akop K <akop87@gmail.com>
2019-02-16 02:42:56 -0700

    populating commission work

--- models/commission.js
+++ models/commission.js
@@ -1,24 +1,30 @@
 module.exports = function(sequelize, DataTypes) {
-  var CommissionRequest = sequelize.define("CommissionRequest", {
-    name: {
-      type: DataTypes.STRING,
-      notNull: true
+  var CommissionRequest = sequelize.define(
+    "CommissionRequest",
+    {
+      name: {
+        type: DataTypes.STRING,
+        notNull: true
+      },
+      requestDetails: DataTypes.TEXT,
+      referenceImgURL: DataTypes.STRING,
+      phone: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        isEmail: true
+      },
+      medium: {
+        type: DataTypes.STRING,
+        defaultValue: "Digital"
+      },
+      artist: {
+        type: DataTypes.STRING,
+        defaultValue: "CJ Frei"
+      }
     },
-    requestDetails: DataTypes.TEXT,
-    referenceImgURL: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      isEmail: true
-    },
-    medium: {
-      type: DataTypes.STRING,
-      defaultValue: "Digital"
-    },
-    artist: {
-      type: DataTypes.STRING,
-      defaultValue: "CJ Frei"
+    {
+      timestamps: false
     }
-  });
+  );
   return CommissionRequest;
 };
